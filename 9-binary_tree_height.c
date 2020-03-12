e3dcd485a220e6c0697c9baaedba806c727d4758
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-03-12 04:56:33 +0000

    Upload test #4

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,32 +2,31 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_height - height of binary tree.
+ * binary_tree_height - height of binary tree
  * @tree :- the tree.
- * Return - the distance
+ * Return:- zero
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
-
 	if (tree == NULL)
 	{
-		return(-1);
+		return (-1);
 	}
 	else
 	{
 		size_t leftHeight = binary_tree_height(tree->left);
 		size_t rightHeight = binary_tree_height(tree->right);
 
-	       if (leftHeight < rightHeight)
-	       {
-		       return(leftHeight + 1);
-	       }
-	       else
-	       {
-		       return(rightHeight + 1);
-	       }
-
+		if (leftHeight < rightHeight)
+		{
+			return (leftHeight + 1);
+		}
+		else
+		{
+			return (rightHeight + 1);
+		}
 	}
-	return(0);
+	return (0);
 }
