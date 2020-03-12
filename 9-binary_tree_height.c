f8f040b19225ff4d3032a12c47ea04ae90d2f3a8
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-03-12 23:17:41 +0000

    adding new binary functions for 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,29 +4,54 @@
 /**
  * binary_tree_height - height of binary tree
  * @tree :- the tree.
- * Return:- zero
+ * Return:- count.
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	return (new_func(tree) + 1);
+}
+
+/**
+ * new_func - that takes left and right leafs in for calculation
+ * @tree: given tree
+ * Return:- the max number between two leafs
+ */
+
+size_t new_func(const binary_tree_t *tree)
 {
 
 	if (tree == NULL)
 	{
 		return (-1);
 	}
+	binary_tree_height(tree->left);
+	binary_tree_height(tree->right);
+
+	return (max(binary_tree_height(tree->left),
+		    binary_tree_height(tree->right)));
+}
+
+/**
+ * max - find the max betwee two numbers
+ * @num1: first given integer
+ * @num2: second given integer
+ * Return:- nothing.
+ */
+
+int max(int num1, int num2)
+{
+	if (num1 > num2)
+	{
+		return (num1);
+	}
 	else
 	{
-		size_t leftHeight = binary_tree_height(tree->left);
-		size_t rightHeight = binary_tree_height(tree->right);
-
-		if (leftHeight < rightHeight)
-		{
-			return (leftHeight + 1);
-		}
-		else
-		{
-			return (rightHeight + 1);
-		}
+		return (num2);
 	}
 	return (0);
 }
