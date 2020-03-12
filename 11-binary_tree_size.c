2cfa9d43b272e4071e816193bb6ee3470dfcf13e
ACholberton <1103@holbertonschool.com>
2020-03-12 20:30:47 +0000

    fixed betty style issues

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_size - measures the size of a binary tree
+ *binary_tree_size-measures the size of a binary tree
  *@tree: is th ebinary tree
- *Returns: the size of the tree
+ *Return: the size of the tree
  */
 
 
@@ -15,6 +15,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	return (binary_tree_size(tree->right) + binary_tree_size(tree->left)
 		+ 1);
-	/* measures the right and left sides of the binary tree and increments
-	   +1 for each level */
+/* measures the right and left sides of the binary tree */
 }
