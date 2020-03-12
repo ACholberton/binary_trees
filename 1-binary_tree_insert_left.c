aa6e5f0f87d12d9a1f40f943f73f7a4c6246552a
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-03-12 05:04:38 +0000

    Upload test # 2 Fixed comments

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - adds a value to the left of the side of the parent.
- *@parent: is the root.
- *@value: is the value added.
- *Return: the added node.
+ * binary_tree_insert_left - adds a value to the left of the side of the parent
+ * @parent: is the root.
+ * @value: is the value added.
+ * Return: the added node.
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
@@ -15,18 +15,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (node == NULL || parent == NULL)
 		return (NULL);
 
-	// Checks if malloc was succesfull//
-
 	node->n = value;
 	node->parent = parent;
 
-	// assigns value to the root//
 
 	if (parent->left)
 	{
 		parent->left->parent = node;
 	}
-	// Added node on the left child//
 
 	node->right = NULL;
 	node->left = parent->left;
