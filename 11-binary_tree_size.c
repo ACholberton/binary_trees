bbdb66ef5b5cb0ee3de9564d62bc63df2c50170d
ACholberton <1103@holbertonschool.com>
2020-03-12 20:23:55 +0000

    Created a function that measures the size of the binary tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_size - measures the size of a binary tree
+ *@tree: is th ebinary tree
+ *Returns: the size of the tree
+ */
+
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	/* checks if the tree exists */
+
+	return (binary_tree_size(tree->right) + binary_tree_size(tree->left)
+		+ 1);
+	/* measures the right and left sides of the binary tree and increments
+	   +1 for each level */
+}
