5cbe75d35b0b38b6b9a8964ab1939eb44b58f0a6
ACholberton <1103@holbertonschool.com>
2020-03-13 04:02:19 +0000

    added a counter that increments everytime it moves throught the binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,49 +9,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return (-1);
-	}
-	return (new_func(tree));
-}
-
-/**
- * new_func - that takes left and right leafs in for calculation
- * @tree: given tree
- * Return:- the max number between two leafs
- */
-
-size_t new_func(const binary_tree_t *tree)
-{
+	size_t height = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	binary_tree_height(tree->left);
-	binary_tree_height(tree->right);
 
-	return (max(binary_tree_height(tree->left),
-		    binary_tree_height(tree->right)) + 1);
-}
-
-/**
- * max - find the max betwee two numbers
- * @num1: first given integer
- * @num2: second given integer
- * Return:- nothing.
- */
-
-int max(int num1, int num2)
-{
-	if (num1 > num2)
-	{
-		return (num1);
-	}
-	else
-	{
-		return (num2);
-	}
-	return (0);
+	if (tree->right != NULL)
+		height = binary_tree_height(tree->right) + 1;
+	if (tree->left != NULL)
+		height = binary_tree_height(tree->left) + 1;
+	return (height);
 }
