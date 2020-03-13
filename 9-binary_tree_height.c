d8403114a9a9c688c116ab0842ca03ae542a5e1e
ACholberton <1103@holbertonschool.com>
2020-03-13 05:03:10 +0000

    added second index to have right and left counters

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,18 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
+	size_t height_left = 0;
+	size_t height_right = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->right != NULL)
-		height = binary_tree_height(tree->right) + 1;
+		height_right = binary_tree_height(tree->right) + 1;
 	if (tree->left != NULL)
-		height = binary_tree_height(tree->left) + 1;
-	return (height);
+		height_left = binary_tree_height(tree->left) + 1;
+	if (height_left > height_right)
+		return (height_left);
+	else
+		return (height_right);
 }
