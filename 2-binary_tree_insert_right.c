1325b77e0ed4752f45c3d31451caf38564385465
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-03-13 20:50:07 +0000

    Upload test #3, removed malloc

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,25 +11,23 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = malloc(sizeof(binary_tree_t));
-
-	if (node == NULL || parent == NULL)
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
-	/* checks if malloc was succesfull */
-	node->n = value;
-	node->parent = parent;
-	/* assigns value to the root */
+
+	node = binary_tree_node(parent, value);
 
 	if (parent->right)
 	{
-		parent->right->parent = node;
-	}
-	/* added new node to the right child */
-	node->left = NULL;
-	node->right = parent->right;
-	parent->right = node;
 
+		node->right = parent->right;
+		node->right->parent = node;
+		parent->right = node;
+	}
+	else
+	{
+		parent->right = node;
+	}
 	return (node);
 }
