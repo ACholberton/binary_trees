3f85815b143820be4fca6b6b4a3970fba4699ebc
ACholberton <1103@holbertonschool.com>
2020-03-13 04:21:23 +0000

    added edge case if the childs are null

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height = 0;
 
-	if (tree == NULL)
+	if (tree == NULL || tree->right == NULL || tree->left == NULL)
 		return (0);
 
 	if (tree->right != NULL)
