8c952eadec1db2b3e8d886a4ebac7896af7c1cdc
ACholberton <1103@holbertonschool.com>
2020-03-13 20:13:27 +0000

    re created task 1 with different aproach

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,4 +1,4 @@
-#include "binary_trees.h"
+ #include "binary_trees.h"
 /**
  * binary_tree_insert_left - adds a value to the left of the side of the parent
  * @parent: is the root.
@@ -10,23 +10,19 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = malloc(sizeof(binary_tree_t));
-
-	if (node == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	node->n = value;
-	node->parent = parent;
-
+	node = binary_tree_node(parent, value);
 
 	if (parent->left)
 	{
-		parent->left->parent = node;
+		node->left = parent->left;
+		node->left->parent = node;
+		parent->left = node;
 	}
-
-	node->right = NULL;
-	node->left = parent->left;
-	parent->left = node;
+	else
+		parent->left = node;
 
 	return (node);
 }
