797ed9e5cf1cf578600eb05d37493043b03f6f93
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-03-14 01:57:03 +0000

    Upload test #4 adding edge case

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,7 +29,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 	size_t right = 0;
 	size_t left = 0;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
 	right = binary_tree_height_2(tree->right);
