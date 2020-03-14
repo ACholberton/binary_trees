bb6857244c4d1036d3d76e174c3f72e4faead3e7
ACholberton <1103@holbertonschool.com>
2020-03-14 01:39:56 +0000

    made changes again

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,37 +8,16 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-<<<<<<< HEAD
-=======
-	int counter_1;
-	int counter_2;
-
->>>>>>> 901f369ce9b0e9facb3056d08698dfab330500cc
 	/* check if the tree exists */
 	if (tree == NULL)
 		return (0);
 
 	/* checks if the left side and right side are NULL */
 	if (tree->left == NULL && tree->right == NULL)
-<<<<<<< HEAD
 		return (1);
-=======
-		return (0);
 
->>>>>>> 901f369ce9b0e9facb3056d08698dfab330500cc
-
-
-<<<<<<< HEAD
 	if (tree->left != NULL && tree->right != NULL)
 		return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
 	return (0);
-=======
-	if (counter_1 == 1 && counter_2 == 1)
-		return (1);
-	if (counter_1 && counter_2)
-		return (0);
-	else
-		return (0);
->>>>>>> 901f369ce9b0e9facb3056d08698dfab330500cc
 }
